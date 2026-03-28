0e5773b5dddbe5e719be5458b34bc7a45e9c628b
SaraRadulovic003 <radulovics003@gmail.com>
2026-03-28 11:08:27 +0100

    Izmenjene klase Autor i Knjiga dodavanjem hashCode, equals i toString metoda

--- src/biblioteka/Autor.java
+++ src/biblioteka/Autor.java
@@ -1,5 +1,7 @@
 package biblioteka;
 
+import java.util.Objects;
+
 public class Autor {
 	private String ime;
 	private String prezime;
@@ -15,6 +17,25 @@ public class Autor {
 	}
 	public void setPrezime(String prezime) {
 		this.prezime = prezime;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(ime, prezime);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(ime, other.ime) && Objects.equals(prezime, other.prezime);
+	}
+	@Override
+	public String toString() {
+		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
 	}
 	
 	
