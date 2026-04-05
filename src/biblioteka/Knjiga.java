2d1d95857144c2725e03b92a3ed8ead18f6e9218
SaraRadulovic003 <radulovics003@gmail.com>
2026-04-05 10:32:37 +0200

    Uvedena logicka kontrola u klasu Knjiga

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -10,42 +10,70 @@ public class Knjiga {
 	private List<Autor> autori = new ArrayList<Autor>();
 	private String izdavac;
 	private int izdanje;
-	
-	
+
 	public String getNaslov() {
 		return naslov;
 	}
+
 	public void setNaslov(String naslov) {
+		if (naslov == null) {
+			throw new NullPointerException("NASLOV NE SME BITI NULL");
+		}
+		if (naslov.isEmpty()) {
+			throw new IllegalArgumentException("NASLOV NE SME BITI PRAZAN");
+		}
 		this.naslov = naslov;
 	}
+
 	public long getIsbn() {
 		return isbn;
 	}
+
 	public void setIsbn(long isbn) {
+		if (isbn <= 0) {
+			throw new IllegalArgumentException("ISBN MORA BITI VECI OD NULE");
+		}
 		this.isbn = isbn;
 	}
+
 	public List<Autor> getAutori() {
 		return autori;
 	}
+
 	public void setAutori(List<Autor> autori) {
 		this.autori = autori;
 	}
+
 	public String getIzdavac() {
 		return izdavac;
 	}
+
 	public void setIzdavac(String izdavac) {
+		if (izdavac == null) {
+			throw new NullPointerException("IZDAVAC NE SME BITI NULL");
+		}
+		if (izdavac.isEmpty()) {
+			throw new IllegalArgumentException("IZDAVAC NE SME BITI PRAZAN");
+		}
 		this.izdavac = izdavac;
 	}
+
 	public int getIzdanje() {
 		return izdanje;
 	}
+
 	public void setIzdanje(int izdanje) {
+		if (izdanje <= 0) {
+			throw new IllegalArgumentException("IZDANJE MORA BITI VECE OD NULE");
+		}
 		this.izdanje = izdanje;
 	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(autori, isbn, izdanje, izdavac, naslov);
+		return Objects.hash(isbn);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -55,14 +83,13 @@ public class Knjiga {
 		if (getClass() != obj.getClass())
 			return false;
 		Knjiga other = (Knjiga) obj;
-		return Objects.equals(autori, other.autori) && isbn == other.isbn && izdanje == other.izdanje
-				&& Objects.equals(izdavac, other.izdavac) && Objects.equals(naslov, other.naslov);
+		return isbn == other.isbn;
 	}
+
 	@Override
 	public String toString() {
 		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
 				+ ", izdanje=" + izdanje + "]";
 	}
-	
-	
+
 }
